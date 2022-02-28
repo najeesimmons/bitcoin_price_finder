4039dbfa2a3687a633726cc6e778ff903db73dc2
najeesimmons <najee.bryant.simmons@gmail.com>
2022-02-28 12:48:18 -0500

    fixed basic routing

--- bitcoin_price_finder/src/App.js
+++ bitcoin_price_finder/src/App.js
@@ -1,24 +1,22 @@
 import React from "react";
 import "./App.css";
 //Import route and our components
-import { Route } from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import Currencies from "./pages/Currencies";
 import Main from "./pages/Main";
 import Price from "./pages/Price";
+import Nav from "./components/nav";
 
 function App() {
   // We will use the Route component to specify each route
   return (
     <div className="App">
-      <Route path="/">
-        <Main />
-      </Route>
-      <Route path="/currencies">
-        <Currencies />
-      </Route>
-      <Route path="/price">
-        <Price />
-      </Route>
+      <Nav />
+      <Routes>
+            <Route exact path="/" element={<Main />} />
+            <Route path="/currencies" element={<Currencies />} />
+            <Route path="/price" element={<Price />} />
+      </Routes>
     </div>
   );
 }
